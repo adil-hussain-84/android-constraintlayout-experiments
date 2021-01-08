35f742e62f799ff42d278e79495fd6dc324792de
Adil Hussain <>
2021-01-08 17:18:41 +0000

    (app1) Added a `Flow` helper in `layout/activity_main.xml`.

--- app1/src/main/java/com/tazkiyatech/constraintlayout/experiments/MainActivity.kt
+++ app1/src/main/java/com/tazkiyatech/constraintlayout/experiments/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tazkiyatech.constraintlayout.experiments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
