e6ef49f36a6a78fa85c3d4335348f362529d53e3
Adil Hussain <>
2021-02-05 16:42:41 +0000

    (app2) Added two more buttons in `MainActivity` to help demonstrate how to change constraints at runtime.

--- app2/src/main/java/com/tazkiyatech/constraintlayout/app2/MainActivity.kt
+++ app2/src/main/java/com/tazkiyatech/constraintlayout/app2/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.tazkiyatech.constraintlayout.app2
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val button1: Button
+        get() = findViewById(R.id.button1)
+
+    private val button2: Button
+        get() = findViewById(R.id.button2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showButtonTwo(view: View) {
+        button2.visibility = View.VISIBLE
+        // TODO
+    }
+
+    fun hideButtonTwo(view: View) {
+        button2.visibility = View.GONE
+        // TODO
     }
 }
